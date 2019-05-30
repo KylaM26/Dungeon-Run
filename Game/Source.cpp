bc27f9d353ff35aa39a9dec2363359a71cd5c0de
Lincoln Nowell <lincolnnowell@gmail.com>
2019-05-30 16:57:38 -0500

    Add files via upload

--- Game/Source.cpp
+++ Game/Source.cpp
@@ -1,46 +1,62 @@
-
 #include "SFML/Graphics.hpp"
-
-#include <iostream>
-#include "Game.h"
+//#include "Game.h"
 
 int main()
 {
 	sf::RenderWindow renderWindow(sf::VideoMode(640, 480), "Dungeon Run");
 
+	sf::Texture texture;
+
+	texture.loadFromFile("GameAssets/DungeonTileset.png");
+
+	sf::Sprite sprite(texture);
+
+	sf::Event event;
+	renderWindow.setKeyRepeatEnabled(true);
+
+	//Game* game = new Game(renderWindow);
+
+	//game->Initialize();
+
 	while (renderWindow.isOpen())
 	{
-		sf::Event event;
-		Game* game = new Game(renderWindow);
-
-		sf::Texture texture;
-	/*	if (!texture.loadFromFile("GameAssets/DungeonTileset.png")) {
-			std::cout << "Failed to load texture" << std::endl;
-			return 1;
-		}*/
-
-		sf::Sprite sprite;
-		sprite.setTexture(texture);
-
-//		game->Initialize();
 
 		while (renderWindow.pollEvent(event))
 		{
-	//		game->Update();
+			//game->Update();
 
 			if (event.type == sf::Event::EventType::Closed)
 			{
 				renderWindow.close();
 			}
 
-			renderWindow.clear(sf::Color::White);
-			renderWindow.draw(sprite);
-		//	game->Draw();
+			//set window to random color to check if working
+			if (event.type == sf::Event::EventType::KeyPressed) {
+				if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
+				}
+				else if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
+				}
+				else if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
+				}
+				else if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
+				}
+				else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) {
+				}
+			}
+			else if (event.type == sf::Event::EventType::MouseButtonPressed) {
+				if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
+				}
+				else if (sf::Mouse::isButtonPressed(sf::Mouse::Right)) {
+				}
+			}
 
+			//game->Draw();
+			renderWindow.clear();
+			renderWindow.draw(sprite);
 			renderWindow.display();
 		}
 
-		delete game;
-		game = nullptr;
+		//delete game;
+		//game = nullptr;
 	}
 }
