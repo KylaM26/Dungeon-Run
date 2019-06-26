6442b9c73f8908d1fb797792d1c3778e95195f44
Lincoln Nowell <lincolnnowell@gmail.com>
2019-06-26 00:56:41 -0500

    Add files via upload

--- Game/GameSaver.h
+++ Game/GameSaver.h
@@ -8,12 +8,42 @@
 
 class GameSaver
 {
+private:
+	std::string path;
 public:
-	GameSaver();
+	GameSaver() {
+		path = "saves/Saves.txt";
+	};
 
 	void SaveGame(const Player&);
-	bool overWriteSave(const std::string& path, const Player& player);
+	bool overWriteSave(const Player& player);
+	std::string format(std::string name) {
+		std::string replstr = "";
+		for (auto& letter : name) {
+			if (letter == ' ') {
+				replstr.push_back('_');
+			}
+			else {
+				replstr.push_back(letter);
+			}
+		}
+		return replstr;
+	}
+	std::string deformat(std::string name) {
+		std::string replstr = "";
+		for (auto& letter : name) {
+			if (letter == '_') {
+				replstr.push_back(' ');
+			}
+			else {
+				replstr.push_back(letter);
+			}
+		}
+		return replstr;
+	}
+	void LoadGame(const Player& player);
 
 	~GameSaver();
 };
+
 
---
+++ Game/GameScreen.cpp
@@ -0,0 +1,31 @@
+#include "GameScreen.h"
+
+
+
+GameScreen::GameScreen()
+{
+}
+
+void GameScreen::LoadContent()
+{
+
+}
+
+void GameScreen::UnloadContent()
+{
+
+}
+
+void GameScreen::Update()
+{
+
+}
+
+void GameScreen::Draw(sf::RenderWindow &Window)
+{
+
+}
+
+GameScreen::~GameScreen()
+{
+}
